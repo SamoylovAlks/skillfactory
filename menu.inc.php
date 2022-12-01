5a701f8ac3990d5ca7c826cebdd1934cc268f922
SamoylovAlks <58441547+SamoylovAlks@users.noreply.github.com>
2022-12-01 23:03:32 +0300

    Add files via upload

---
+++ menu.inc.php
@@ -0,0 +1,9 @@
+<nav>
+                <ul>
+                    <li><a href="#"> University </a></li>
+                    <li><a href="#"> p2p </a></li>
+                    <li><a href="#"> What? </a></li>
+                    <li><a href="#"> Do not click </a></li>
+                    <li><a href="#"> Contacts </a></li>
+                </ul>    
+            </nav>
